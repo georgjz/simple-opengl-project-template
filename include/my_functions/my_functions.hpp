6ec9f83e5f208c4b3bf5b1c4d7bc67bb91d221f1
Georg Ziegler <georgjziegler@gmail.com>
2020-06-20 18:03:18 +0200

    Add better subdirectory CMake

---
+++ include/my_functions/my_functions.hpp
@@ -0,0 +1,10 @@
+#ifndef MY_FUNCTIONS_HPP
+#define MY_FUNCTIONS_HPP
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void framebuffer_size_callback( GLFWwindow* window, int width, int height );
+void processInput( GLFWwindow *window );
+
+#endif // MY_FUNCTIONS_HPP
