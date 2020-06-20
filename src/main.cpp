831b9e211445535343aee0a1570c6ee5b86d48b6
Georg Ziegler <georgjziegler@gmail.com>
2020-06-20 15:09:50 +0200

    Add glfw to build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "glm/vec3.hpp"
+#include "GLFW/glfw3.h"
 
 int main()
 {
     glm::vec3 testVector = glm::vec3( 1.0f, 2.0f, 3.0f );
-    std::cout << "Simple test, with glm: " << testVector.z
-                                           << std::endl;
+    std::cout << "Simple test, with glm: " << testVector.z << std::endl;
+    if( !glfwInit() )
+    {
+        std::cout << "Init failed\n";
+    }
+    else
+    {
+        std::cout << "Init worked\n";
+    }
+    glfwTerminate();
     return 0;
 }
